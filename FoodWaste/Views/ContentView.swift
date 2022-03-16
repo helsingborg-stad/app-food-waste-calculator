543782adefbcff7490616f65685ebd96ab13be9a
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2022-03-16 08:22:43 +0100

    added background

--- FoodWaste/Views/ContentView.swift
+++ FoodWaste/Views/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            BackgroundView()
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
