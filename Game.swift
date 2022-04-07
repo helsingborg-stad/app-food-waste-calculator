aeb88691781bd9741d89b620740b10ccc9189327
Jonatan Hanson <jonatan.hanson@gmail.com>
2022-04-07 15:30:37 +0200

    removed duplicate func

--- Game.swift
+++ Game.swift
@@ -19,7 +19,7 @@ class Game: ObservableObject {
     @Published var wasteInputs: [WasteParameter] = []
     
     init(loadTestData: Bool = false) {
-        if (loadTestData) {
+        if loadTestData {
             wasteInputs.append(.gazpacho)
             wasteInputs.append(.soundModerate)
             wasteInputs.append(.rain)
@@ -47,17 +47,6 @@ class Game: ObservableObject {
     
     func removeAllWasteInputs() {
         wasteInputs.removeAll()
-    }
-    
-    func getScoreLevel(score: Int) -> ScoreLevel {
-        switch score {
-        case 0...6:
-            return .low
-        case 7...9:
-            return .medium
-        default:
-            return .high
-        }
     }
     
     func getScoreLevel(score: Int) -> ScoreLevel {
