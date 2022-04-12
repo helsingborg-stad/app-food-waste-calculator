03bdea29618315e018a0c1adb56dd491a4417ddf
Ehsan Zilaei <zzilaei@gmail.com>
2022-04-12 10:25:29 +0200

    Styling foxes for calculator (#23)
    * fixed hight and paddings for calculator
    
    * decreased waste button shadow radius

--- FoodWaste/Views/Calculator/KeyboardWastParamsView.swift
+++ FoodWaste/Views/Calculator/KeyboardWastParamsView.swift
@@ -14,13 +14,17 @@ struct KeyboardWastParamsView: View {
     private func getButtonGroup(wasteParameters: [WasteParameter]) -> some View {
         Group {
             ForEach (wasteParameters) { waste in
+                let lastElement = waste != wasteParameters.last
+                
                 RoundedWasteButton(waste: waste)
+                    .padding(.trailing, lastElement ? 5 : 0)
+                
             }
         }
     }
     
     var body: some View {
-        LazyVGrid(columns: columns, spacing: 7) {
+        LazyVGrid(columns: columns, spacing: 10) {
             getButtonGroup(wasteParameters: [.burger, .gazpacho, .fish])
             getButtonGroup(wasteParameters: [.soundLow, .soundModerate, .soundHigh])
             getButtonGroup(wasteParameters: [.queueShort, .queueModerate, .queueLong])
@@ -32,5 +36,6 @@ struct KeyboardWastParamsView: View {
 struct KeyboardWastParamView_Previews: PreviewProvider {   
     static var previews: some View {
         KeyboardWastParamsView()
+            .environmentObject(Game())
     }
 }
