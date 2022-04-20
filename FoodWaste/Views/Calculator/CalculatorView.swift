522529c9f8cd3dfadef383f490c57ea3e47b1460
zilaei <zzilaei@gmail.com>
2022-04-20 10:13:12 +0200

    fixed display text colors

--- FoodWaste/Views/Calculator/CalculatorView.swift
+++ FoodWaste/Views/Calculator/CalculatorView.swift
@@ -31,7 +31,7 @@ struct CalculatorView: View {
         
         if sum == 0 {
             return Text("calcDisplayGreeting".localized(localization.language))
-                .foregroundColor(Color("CalculatorDisplayGreetingColor"))
+                .foregroundColor(Color("CalculatorDisplayTextColor"))
         }
         
         switch level {
